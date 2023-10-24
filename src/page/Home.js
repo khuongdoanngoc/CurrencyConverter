9a2070b76e190540cce459ef966404ec27e9b3f6
khuongdoanngoc <khuong.dngc@gmail.com>
2023-10-24 17:12:40 +0700

    Updated

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,15 @@
+import './Home.css'
+
 function Home() {
     return ( 
-        <>
-            <span>Container</span>
-        </>
+        <div className='converter'>
+            <div className="title">
+                <h1>CURRENCY CONVERTER</h1>
+            </div>
+            <div className='calculator'>
+
+            </div>
+        </div>
      );
 }
 
