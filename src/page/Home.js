02ecbe1672090dc929387102eeb1463f0788ded4
Doan Ngoc Khuong <93414369+khuongdoanngoc@users.noreply.github.com>
2023-10-25 00:37:55 +0700

    code updated

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,10 +4,25 @@ function Home() {
     return ( 
         <div className='converter'>
             <div className="title">
-                <h1>CURRENCY CONVERTER</h1>
+                <h1>CURRENCY CONVERSION</h1>
             </div>
-            <div className='calculator'>
+            <div className='calculator-container'>
+                <div className='calculator-content'>
+                    <div className='collector'>
+                        <div className='form-group'>
+                            {/* <label>Amount of money</label> */}
+                        </div>
+                        <div className='reverse-icon'>
 
+                        </div>
+                        <div className='form-group'>
+                            {/* <label>Convert to</label> */}
+                        </div>
+                    </div>
+                    <div className='conclusion'>
+
+                    </div>
+                </div>
             </div>
         </div>
      );
