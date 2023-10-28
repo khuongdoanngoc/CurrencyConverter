013c79c9a77afc1eba254ab86980acba6da3ada4
khuongdoanngoc <khuong.dngc@gmail.com>
2023-10-29 02:16:12 +0700

    Updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globalStyles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,4 +1,5 @@
 import './Home.css'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Home() {
     return (
@@ -12,47 +13,61 @@ function Home() {
                         <div className='form-group'>
                             <label>Amount of money</label>
                             <div className='input-group'>
-                                <input />
-                                <div className='national-group'>
-                                    <button className='national-btn'>
+                                <input className='custom-input' />
+                                <Dropdown className='national-group'>
+                                    <Dropdown.Toggle className='national-btn custom-btn' variant={null} id='dropdown-basic'>
+                                        <div className='flag'>
+
+                                        </div>
                                         <div className='ensign'>
 
                                         </div>
                                         <span className='icon'>
-
+                                            <svg width="16" height="16" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path d="M12.771 7.115a.829.829 0 0 0-1.2 0L3 15.686l1.2 1.2 7.971-7.971 7.972 7.971 1.2-1.2-8.572-8.571Z"></path></svg>
                                         </span>
-                                        {/* <span class="tw-icon tw-icon-chevron-up tw-chevron chevron-color bottom tw-icon tw-chevron-up-icon tw-chevron bottom np-select-chevron" aria-hidden="true" role="presentation" data-testid="chevron-up-icon"><svg width="16" height="16" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path d="M12.771 7.115a.829.829 0 0 0-1.2 0L3 15.686l1.2 1.2 7.971-7.971 7.972 7.971 1.2-1.2-8.572-8.571Z"></path></svg></span> */}
-                                    </button>
-
-                                </div>
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
                             </div>
                         </div>
                         <div className='reverse-icon'>
                             <a href='/'>
-                                <svg width="24" height="24" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="m16.629 11.999-1.2-1.2 3.085-3.086H2.572V5.999h15.942L15.43 2.913l1.2-1.2 4.543 4.543a.829.829 0 0 1 0 1.2l-4.543 4.543Zm-9.257-.001 1.2 1.2-3.086 3.086h15.943v1.714H5.486l3.086 3.086-1.2 1.2-4.543-4.543a.829.829 0 0 1 0-1.2l4.543-4.543Z"></path></svg>
+                                <svg width="24" height="24" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path fillRule="evenodd" clipRule="evenodd" d="m16.629 11.999-1.2-1.2 3.085-3.086H2.572V5.999h15.942L15.43 2.913l1.2-1.2 4.543 4.543a.829.829 0 0 1 0 1.2l-4.543 4.543Zm-9.257-.001 1.2 1.2-3.086 3.086h15.943v1.714H5.486l3.086 3.086-1.2 1.2-4.543-4.543a.829.829 0 0 1 0-1.2l4.543-4.543Z"></path></svg>
                             </a>
                         </div>
                         <div className='form-group'>
                             <label>Convert to</label>
                             <div className='input-group'>
-                                <input />
-                                <div className='national-group'>
-                                    <button className='national-btn'>
+                                <input className='custom-input' />
+                                <Dropdown className='national-group'>
+                                    <Dropdown.Toggle className='national-btn custom-btn' variant={null} id='dropdown-basic'>
+                                        <div className='flag'>
+
+                                        </div>
                                         <div className='ensign'>
 
                                         </div>
                                         <span className='icon'>
-
+                                            <svg width="16" height="16" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path d="M12.771 7.115a.829.829 0 0 0-1.2 0L3 15.686l1.2 1.2 7.971-7.971 7.972 7.971 1.2-1.2-8.572-8.571Z"></path></svg>
                                         </span>
-                                        {/* <span class="tw-icon tw-icon-chevron-up tw-chevron chevron-color bottom tw-icon tw-chevron-up-icon tw-chevron bottom np-select-chevron" aria-hidden="true" role="presentation" data-testid="chevron-up-icon"><svg width="16" height="16" fill="currentColor" focusable="false" viewBox="0 0 24 24"><path d="M12.771 7.115a.829.829 0 0 0-1.2 0L3 15.686l1.2 1.2 7.971-7.971 7.972 7.971 1.2-1.2-8.572-8.571Z"></path></svg></span> */}
-                                    </button>
-
-                                </div>
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
                             </div>
                         </div>
                     </div>
                     <div className='conclusion'>
-
+                        <h2>
+                            information
+                        </h2>
                     </div>
                 </div>
             </div>
